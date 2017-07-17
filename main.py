514cb16b295e2bce758a2ce9b3e740c9e00e60c2
buko106 <buko1062000@yahoo.co.jp>
2017-07-17 16:22:34 +0900

    todo: generate()

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ parser.add_argument("--prefix-train", type = str, default="train_", help="defaul
 parser.add_argument("--prefix-test" , type = str, default="test_" , help="default='test_'")
 parser.add_argument("--dataset"     , metavar="DATA",type = str, default="ml-latest-small/",help="default='ml-latest-small/'" )
 parser.add_argument("-o", "--output", metavar="DIR" ,type = str, default="output" , help="default='output/'")
+parser.add_argument("-k",type = int, default=5 , help="default=5. Generate dataset for K-fold cross-validation")
 parser.add_argument("-t", "--timestamp", action="store_true")
 parser.add_argument("-g", "--genre", action="store_true")
 parser.add_argument("--other", action="store_true")
@@ -29,7 +30,6 @@ if not os.path.exists(output):
 # read movie file
 movie_to_genre_set = {}
 all_genre = set()
-all_movie = set()
 with open(moviefile,"r") as f:
     reader = csv.reader(f)
     header = next(reader)
@@ -40,10 +40,11 @@ with open(moviefile,"r") as f:
         genre = set(genre.split("|"))
         assert(len(genre)>0) # a movie at least one genre
         all_genre = all_genre.union(genre)
-        all_movie.add(movie)
         movie_to_genre_set[movie] = genre 
 
 # read rating file
+all_movie = set()
+all_user  = set()
 data = []
 with open(ratingfile,"r") as f:
     reader = csv.reader(f)
@@ -51,13 +52,14 @@ with open(ratingfile,"r") as f:
     print("header =",header)
     for row in reader:
         assert(len(row)==4)
+        user,movie,rating,timestamp = row
+        all_movie.add(movie)
+        all_user.add(user)
         data += [row]
         
 # get miminum timestamp
 minTime = min([ int(t) for _,_,_,t in data ])
 
-# get all user
-all_user = set( [ u for u,_,_,_ in data ] )
 # user to id
 user_to_id = { user:str(i) for i,user in enumerate(all_user) }
 print("num of users =",len(user_to_id))
@@ -68,3 +70,45 @@ print("genres =",genre_to_id)
 
 # movie to id
 movie_to_id = { movie:str(i) for i,movie in enumerate(all_movie) }
+print("num of movies =",len(movie_to_id))
+
+
+############################################################
+# regenerate data
+############################################################
+data = [ [user_to_id[user],
+          movie_to_id[movie],
+          movie_to_genre_set[movie],
+          rating,
+          str(int(timestamp)-minTime) ]
+         for user,movie,rating,timestamp in data ]
+
+# create history
+history = { id:[] for _,id in user_to_id.items() }
+for u,m,_,_,t in data:
+    history[u] += [ (t,m) ]
+for _,v in history.items():
+    v.sort(key = lambda x:x[0]) # sort by timestamp
+
+
+# data for cross-validation
+random.shuffle(data)
+size_of_bag = len(data)//args.k
+print("size_of_bag =",size_of_bag)
+T = [ data[i*size_of_bag:(i+1)*size_of_bag] for i in range(args.k) ]
+
+def generate( filename, data ):
+    with open(filename,"w") as out:
+        pass
+    
+for i in range(args.k):
+    train = []
+    test  = []
+    for id,t in enumerate(T):
+        if i == id:
+            test  += t
+        else:
+            train += t
+    
+    generate( os.path.join(output,"train_%02d.txt"%i),train)
+    generate( os.path.join(output, "test_%02d.txt"%i),test)
